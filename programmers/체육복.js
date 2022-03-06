3f69c545ab687867fd8a3cf374a102d0d8af8bf3
jynam <njy1007@gmail.com>
2022-03-07 01:04:44 +0900

    4차 풀이

--- programmers/체육복.js
+++ programmers/체육복.js
@@ -1,33 +1,19 @@
 function solution(n, lost, reserve) {
-  for (let i = 0; i < reserve.length; i++) {
-    let lostIndex = lost.indexOf(reserve[i]);
-    if (lostIndex > -1) {
-      reserve.splice(i, 1);
-      i--;
-      lost.splice(lostIndex, 1);
-    }
-  }
-  let isFirst = false;
-  for (let i = 0; i < lost.length; i++) {
-    let reserveIndexFront = reserve.indexOf(lost[i] - 1);
-    let reserveIndexBack = reserve.indexOf(lost[i] + 1);
-    if (reserveIndexFront > -1) {
-      lost.splice(i, 1);
-      i--;
-      reserve.splice(reserveIndexFront, 1);
-      if (!isFirst) isFirst = true;
-    } else if (reserveIndexBack > -1) {
-      lost.splice(i, 1);
-      i--;
-      reserve.splice(reserveIndexBack, 1);
-      if (!isFirst) {
-        isFirst = true;
-        const temp = reserveIndexFront;
-        reserveIndexFront = reserveIndexBack;
-        reserveIndexBack = temp;
+  lost.sort();
+  reserve.sort();
+  let lPointer = 0;
+  let rPointer = 0;
+  while (lPointer < lost.length) {
+    while (rPointer < reserve.length) {
+      if (Math.abs(lost[lPointer] - reserve[rPointer]) > 1) {
+        rPointer++;
+      } else {
+        lost.splice(lPointer, 1);
+        reserve.splice(rPointer, 1);
+        lPointer = 0;
+        rPointer = 0;
+        break;
       }
-    } else {
-      isFirst = false;
     }
   }
   return n - lost.length;
