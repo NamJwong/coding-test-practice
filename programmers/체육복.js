618f988a4cfd269a623eb0c1631149233b1a162d
jynam <njy1007@gmail.com>
2022-03-07 02:05:35 +0900

    5차 풀이

--- programmers/체육복.js
+++ programmers/체육복.js
@@ -1,20 +1,35 @@
 function solution(n, lost, reserve) {
-  lost.sort();
-  reserve.sort();
+  lost.sort(function (a, b) {
+    return a - b;
+  });
+  reserve.sort(function (a, b) {
+    return a - b;
+  });
   let lPointer = 0;
   let rPointer = 0;
+  let count = 0;
   while (lPointer < lost.length) {
+    console.log("--------lPointer:" + lPointer + "-------------");
+    console.log("lost", lost.join(", "));
     while (rPointer < reserve.length) {
-      if (Math.abs(lost[lPointer] - reserve[rPointer]) > 1) {
-        rPointer++;
+      const difference = lost[lPointer] - reserve[rPointer];
+      console.log(`${lost[lPointer]}-${reserve[rPointer]}=${difference}`);
+      if (Math.abs(difference) > 1) {
+        if (difference > 0) {
+          rPointer++;
+        } else {
+          break;
+        }
       } else {
-        lost.splice(lPointer, 1);
-        reserve.splice(rPointer, 1);
-        lPointer = 0;
-        rPointer = 0;
+        console.log(rPointer);
+        rPointer++;
+        count++;
         break;
       }
     }
+    if (rPointer >= reserve.length) break;
+    lPointer++;
+    console.log("lPointer++할때의 rPointer", rPointer);
   }
-  return n - lost.length;
+  return n - (lost.length - count);
 }
