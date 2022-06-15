cd9d6c75f22c39492316ce6002d4d28fb59327a9
jynam <njy1007@gmail.com>
2022-06-16 03:25:55 +0900

    feat: searchWord state에 저장해서 검색 구현

--- 과제/2022-Dev-Matching/src/components/SearchInput.js
+++ 과제/2022-Dev-Matching/src/components/SearchInput.js
@@ -3,12 +3,14 @@ import { $, on } from '../utils/DOM.js';
 
 export default class SearchInput extends Component {
   template() {
-    return `<input class="SearchInput__input" type="text" placeholder="프로그램 언어를 입력하세요.">`;
+    console.log('te', this.props.searchWord);
+    return `<input class="SearchInput__input" type="text" placeholder="프로그램 언어를 입력하세요." autofocus value="${this.props.searchWord}">`;
   }
 
   setEvent() {
     const { onChange } = this.props;
-    this.addEvent('change', '.SearchInput__input', async () => {
+    this.addEvent('input', '.SearchInput__input', async (e) => {
+      if (!$('.SearchInput__input').value) return;
       // api 함수 만들기
       fetch(
         `https://wr4a6p937i.execute-api.ap-northeast-2.amazonaws.com/dev/languages?keyword=${
@@ -18,8 +20,18 @@ export default class SearchInput extends Component {
       )
         .then((response) => response.json())
         .then((data) => {
-          onChange(data);
+          console.log(e.target);
+          console.log('dds', e.target.value);
+          onChange(data, e.target.value);
+          console.log('dd', e.target.value);
+          console.log('d', this.props.searchWord);
         });
     });
+  }
+
+  mounted() {
+    $('.SearchInput__input').value = '';
+    $('.SearchInput__input').focus();
+    $('.SearchInput__input').value = this.props.searchWord;
   }
 }
