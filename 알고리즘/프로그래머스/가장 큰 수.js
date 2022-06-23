754d9c14f334bdb3938cdf5fba86fa59367a988c
jynam <njy1007@gmail.com>
2022-06-24 04:02:38 +0900

    2차 풀이 - 정답(풀이 서치함)

--- 알고리즘/프로그래머스/가장 큰 수.js
+++ 알고리즘/프로그래머스/가장 큰 수.js
@@ -1,22 +1,5 @@
 function solution(numbers) {
-  const getPermutations = (arr, selectNumber) => {
-    const results = [];
-    if (selectNumber === 1) return arr.map((value) => [value]);
-    arr.forEach((fixed, index, origin) => {
-      const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
-      const permutations = getPermutations(rest, selectNumber - 1);
-      const attached = permutations.map((permutation) => [
-        fixed,
-        ...permutation,
-      ]);
-      results.push(...attached);
-    });
-    return results;
-  };
-
-  const permutations = getPermutations(numbers, numbers.length).map(
-    (permutation) => permutation.join('')
-  );
-  permutations.sort((a, b) => b - a);
-  return permutations[0];
+  let strings = numbers.map((num) => num.toString());
+  const answer = strings.sort((a, b) => b + a - (a + b)).join('');
+  return answer[0] === '0' ? '0' : answer;
 }
