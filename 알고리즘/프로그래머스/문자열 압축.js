2fba99f1b5c5478e94dd23d6a713d18bea52a20e
jynam <njy1007@gmail.com>
2022-06-28 18:40:43 +0900

    리팩토링

--- 알고리즘/프로그래머스/문자열 압축.js
+++ 알고리즘/프로그래머스/문자열 압축.js
@@ -1,24 +1,21 @@
 function solution(s) {
-  let answer = s.length;
+  let minLength = s.length;
   for (let i = 1; i < s.length; i++) {
     let sliced = '';
     let compressed = [];
     for (let j = 0; j + i - 1 < s.length; j += i) {
       sliced = s.substring(j, j + i);
-      if (
-        compressed[compressed.length - 1] &&
-        compressed[compressed.length - 1][1] === sliced
-      )
-        compressed[compressed.length - 1][0]++;
+      last = compressed[compressed.length - 1];
+      if (last && last[1] === sliced) last[0]++;
       else {
         compressed.push([1, sliced]);
       }
     }
-    answer = Math.min(
-      answer,
-      compressed.flatMap((c) => (c[0] < 2 ? c[1] : c)).join('').length +
-        (s.length % i)
-    );
+    const compressedStr = compressed
+      .flatMap((c) => (c[0] < 2 ? c[1] : c))
+      .join('');
+    const restLength = s.length % i;
+    minLength = Math.min(minLength, compressedStr.length + restLength);
   }
-  return answer;
+  return minLength;
 }
