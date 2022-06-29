b29959a33fd269e4e57079f3a5975643e4d2bf24
jynam <njy1007@gmail.com>
2022-06-29 14:55:45 +0900

    풀이 코드

---
+++ 알고리즘/프로그래머스/수식 최대화.js
@@ -0,0 +1,45 @@
+function solution(expression) {
+  let operatorCopy = expression.match(new RegExp('[-|*|+]', 'g'));
+  let operandCopy = expression.split(new RegExp('[-|*|+]', 'g'));
+  const priorityCombination = [
+    ['-', '*', '+'],
+    ['-', '+', '*'],
+    ['*', '-', '+'],
+    ['*', '+', '-'],
+    ['+', '*', '-'],
+    ['+', '-', '*'],
+  ];
+  let answer = 0;
+  let operator;
+  let operand;
+  for (const priority of priorityCombination) {
+    operator = [...operatorCopy];
+    operand = [...operandCopy];
+    for (const p of priority) {
+      for (let i = 0; i < operator.length; i++) {
+        if (operator[i] === p) {
+          operand.splice(
+            i,
+            2,
+            operate(+operand[i], +operand[i + 1], operator[i])
+          );
+          operator.splice(i, 1);
+          i = -1;
+        }
+      }
+    }
+    answer = Math.max(answer, Math.abs(operand[0]));
+  }
+  return answer;
+}
+
+const operate = (a, b, operand) => {
+  switch (operand) {
+    case '*':
+      return a * b;
+    case '-':
+      return a - b;
+    case '+':
+      return a + b;
+  }
+};
