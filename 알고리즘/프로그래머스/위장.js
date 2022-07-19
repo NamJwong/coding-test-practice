02b5826b5b7a8a70dec14676af6b9d6996a4f95a
jynam <njy1007@gmail.com>
2022-07-19 19:20:20 +0900

    풀이 코드

--- 알고리즘/프로그래머스/위장.js
+++ 알고리즘/프로그래머스/위장.js
@@ -1,21 +1,7 @@
 function solution(clothes) {
-  let answer = 0;
   const map = new Map();
   for (const clothe of clothes) {
-    if (map.get(clothe[1])) map.get(clothe[1]).push(clothe[0]);
-    else map.set(clothe[1], [clothe[0]]);
+    map.set(clothe[1], map.get(clothe[1]) + 1 || 1);
   }
-  const arr = Array.from(map);
-  const temp = arr.reduce((acc, curr) => acc * curr[1].length, 1);
-
-  const getFactorial = (num) => {
-    let counter = 1;
-    for (let i = 2; i <= num; i++) counter *= i;
-    return counter;
-  };
-
-  for (let i = 1; i <= arr.length; i++) {
-    answer += getFactorial(i) * temp;
-  }
-  return answer;
+  return Array.from(map).reduce((acc, curr) => (curr[1] + 1) * acc, 1) - 1;
 }
