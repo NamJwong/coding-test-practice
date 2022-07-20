d960e570c5bfcc9d03c7bcbee1e7c4772d9c86a9
남주영 <njy1007@gmail.com>
2022-07-21 02:07:43 +0900

    Update 구름 입력.js

--- 스켈레톤/구름 입력.js
+++ 스켈레톤/구름 입력.js
@@ -5,30 +5,30 @@
 // 3 1 4 1 5 9
 
 const solution = (N, problems) => {
-	// 풀이 코드 작성 (프로그래머스와 같은 방식)
-}
+  // 풀이 코드 작성 (프로그래머스와 같은 방식)
+};
 
-const readline = require("readline");
+const readline = require('readline');
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 let N;
 let problems = [];
 
-rl.on("line", function(line) {
-	if (!N) {
+rl.on('line', function (line) {
+  if (!N) {
     N = +line;
   } else {
-		problems = line.split('').map(c=>+c);
-		rl.close();
-		if (problems.length === N) { // count가 N일때 rl.close()를 호출해준다.
-			console.log(solution(N, problems));
-		}else {
-			console.log('잘못된 입력입니다.');
-		}
-	}
-}).on("close", function() {
-	process.exit();
+    problems = line.split('').map((c) => +c);
+    rl.close();
+    if (problems.length === N) {
+      console.log(solution(N, problems));
+    } else {
+      console.log('잘못된 입력입니다.');
+    }
+  }
+}).on('close', function () {
+  process.exit();
 });
