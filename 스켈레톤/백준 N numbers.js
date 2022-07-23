d8925afcf1e978afaf868cb115a9b60c25c908ae
jynam <njy1007@gmail.com>
2022-07-24 01:32:49 +0900

    입력 스켈레톤

---
+++ 스켈레톤/백준 N numbers.js
@@ -0,0 +1,8 @@
+const solution = (numbers) => {};
+
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(file).toString().trim().split('\n');
+const [N, numbers] = input;
+
+console.log(solution(numbers.split(' ').map((n) => +n)));
