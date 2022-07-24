32ff894dbc42b80eb5ef6cfb89d3d81dd7769a5c
jynam <njy1007@gmail.com>
2022-07-25 02:46:46 +0900

    풀이 코드

--- 알고리즘/백준/1520 내리막 길/main.js
+++ 알고리즘/백준/1520 내리막 길/main.js
@@ -1,39 +1,26 @@
 const solution = (N, M, map) => {
-  let cnt = 0;
   const moves = [
     [0, 1],
     [1, 0],
     [0, -1],
     [-1, 0],
   ];
-  let visited = Array.from(Array(N), () => Array(M).fill(false));
-  const initVisited = () => {
-    visited = Array.from(Array(N), () => Array(M).fill(false));
-  };
+  let dp = Array.from(Array(N), () => Array(M).fill(null));
   const dfs = (x, y) => {
-    if (x === N - 1 && y === M - 1) {
-      initVisited();
-      return ++cnt;
-    }
-    visited[x][y] = true;
-    let isEnd = true;
-    for (let i = 0; i < 4; i++) {
-      const xToMove = x + moves[i][0];
-      const yToMove = y + moves[i][1];
-      if (
-        map[xToMove] &&
-        map[xToMove][yToMove] &&
-        map[xToMove][yToMove] < map[x][y] &&
-        !visited[xToMove][yToMove]
-      ) {
-        isEnd = false;
-        dfs(xToMove, yToMove, map[x][y]);
+    if (x === N - 1 && y === M - 1) return 1;
+    if (dp[x][y] === null) {
+      dp[x][y] = 0;
+      for (let i = 0; i < 4; i++) {
+        const nextX = x + moves[i][0];
+        const nextY = y + moves[i][1];
+        if (map[nextX] && map[nextX][nextY] && map[x][y] > map[nextX][nextY]) {
+          dp[x][y] += dfs(nextX, nextY);
+        }
       }
     }
-    if (isEnd) initVisited();
+    return dp[x][y];
   };
-  dfs(0, 0);
-  return cnt;
+  return dfs(0, 0);
 };
 
 const fs = require('fs');
