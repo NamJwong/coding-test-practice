f3ac245f399ed04ecd3abe3980d6932b66d54d6a
jynam <njy1007@gmail.com>
2022-07-30 03:26:09 +0900

    스켈레톤 코드 형식 변경

--- 스켈레톤/백준 N M numbers/main.js
+++ 스켈레톤/백준 N M numbers/main.js
@@ -2,12 +2,12 @@ const solution = (N, M, numbers) => {};
 
 const fs = require('fs');
 const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(file).toString().trim().split('\n');
-const [size, ...numbers] = input;
+const lines = fs.readFileSync(file).toString().trim().split('\n');
+const [line, ...numbers] = lines;
 
 console.log(
   solution(
-    ...size.split(' ').map((c) => +c),
+    ...line.split(' ').map((c) => +c),
     numbers.map((s) => s.split(' ').map((c) => +c))
   )
 );
