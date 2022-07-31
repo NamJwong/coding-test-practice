041a16f4f83a5e252b2f029362449214256b4247
남주영 <njy1007@gmail.com>
2022-08-01 03:58:12 +0900

    Delete 백준 numbers.js

--- 스켈레톤/백준 numbers.js
+++
@@ -1,8 +0,0 @@
-const solution = (numbers) => {};
-
-const fs = require('fs');
-const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(file).toString().trim().split('\n');
-const [N, ...numbers] = input;
-
-console.log(solution(numbers.map((n) => +n)));
