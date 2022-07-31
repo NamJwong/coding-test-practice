c69d80dd75d9772ff96e53a79538e1ee3a62d305
jynam <njy1007@gmail.com>
2022-08-01 03:30:08 +0900

    풀이 코드

--- 알고리즘/백준/1697 숨바꼭질/main.js
+++ 알고리즘/백준/1697 숨바꼭질/main.js
@@ -1,14 +1,26 @@
 const solution = (N, K) => {
-  const dfs = (num, cnt) => {
-    if (num === K) {
-      return cnt;
+  const queue = [];
+  let queueIdx = 0;
+  const visited = Array(Math.max(N, K) + 1).fill(false);
+  queue.push([N, 0]);
+  while (queue.length > queueIdx) {
+    const pos = queue[queueIdx];
+    if (pos[0] === K) {
+      return pos[1];
     }
-    dfs(num - 1, cnt + 1);
-    dfs(num + 1, cnt + 1);
-    dfs(num * 2, cnt + 1);
-  };
-
-  return dfs(N, 1);
+    if (visited[pos[0]]) {
+      queueIdx++;
+      continue;
+    }
+    queueIdx++;
+    visited[pos[0]] = true;
+    pos[0] > 0 && queue.push([pos[0] - 1, pos[1] + 1]);
+    pos[0] < 100000 && N < K && queue.push([pos[0] + 1, pos[1] + 1]);
+    pos[0] <= 50000 &&
+      N < K &&
+      pos[0] > 0 &&
+      queue.push([pos[0] * 2, pos[1] + 1]);
+  }
 };
 
 const fs = require('fs');
