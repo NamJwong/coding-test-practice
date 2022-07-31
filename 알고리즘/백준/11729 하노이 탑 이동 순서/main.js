ef8c3c84504ae1891dba3c8101a45d3d686849d1
jynam <njy1007@gmail.com>
2022-08-01 00:33:44 +0900

    풀이 코드

--- 알고리즘/백준/11729 하노이 탑 이동 순서/main.js
+++ 알고리즘/백준/11729 하노이 탑 이동 순서/main.js
@@ -1,12 +1,14 @@
 const solution = (N) => {
+  const moves = [];
   const hanoi = (num, from, to, other) => {
-    if (num === 1) return console.log(from, to);
+    if (num === 1) return moves.push(`${from} ${to}`);
     hanoi(num - 1, from, other, to);
-    console.log(from, to);
+    moves.push(`${from} ${to}`);
     hanoi(num - 1, other, to, from);
   };
   console.log(2 ** N - 1);
   hanoi(N, 1, 3, 2);
+  console.log(moves.join('\n'));
 };
 
 const fs = require('fs');
