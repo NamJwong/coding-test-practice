cf0c7746166cef76c33697f5c44efb42473a3a4f
jynam <njy1007@gmail.com>
2022-08-25 22:34:08 +0900

    feat: Suggestion 렌더링

--- 과제/2022-Dev-Matching-1/2/src/components/App.js
+++ 과제/2022-Dev-Matching-1/2/src/components/App.js
@@ -1,17 +1,45 @@
 import { getLanguagesByKeyword } from '../api.js';
 import SearchInput from './SearchInput.js';
+import Suggestion from './Suggestion.js';
 
 export default function App({ $target }) {
   this.state = {
-    searchedLanguageList: [],
-    selectedLanguageList: [],
+    searchedLanguages: [],
+    selectedLanguages: [],
+  };
+
+  this.setState = (nextState) => {
+    this.state = {
+      ...this.state,
+      ...nextState,
+    };
+    suggestion.setState({
+      items: this.state.searchedLanguages,
+    });
   };
 
   const searchInput = new SearchInput({
     $target,
     initialState: { keyword: '' },
     onChange: async (keyword) => {
-      const languages = await getLanguagesByKeyword(keyword);
+      if (keyword.length === 0) {
+        this.setState({
+          searchedLanguages: [],
+        });
+      } else {
+        const languages = await getLanguagesByKeyword(keyword);
+        console.log(this);
+        this.setState({
+          searchedLanguages: languages,
+        });
+      }
+    },
+  });
+
+  const suggestion = new Suggestion({
+    $target,
+    initialState: {
+      items: [],
     },
   });
 }
