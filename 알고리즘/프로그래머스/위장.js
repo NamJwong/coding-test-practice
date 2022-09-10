d431f64f60196edaf6402fa5e23bff37f7876718
남주영 <njy1007@gmail.com>
2022-09-10 15:19:31 +0900

    Update 위장.js

--- 알고리즘/프로그래머스/위장.js
+++ 알고리즘/프로그래머스/위장.js
@@ -1,7 +1,7 @@
 function solution(clothes) {
   const map = new Map();
-  for (const clothe of clothes) {
-    map.set(clothe[1], map.get(clothe[1]) + 1 || 1);
+  for (const c of clothes) {
+    map.set(c[1], map.get(c[1]) + 1 || 1);
   }
   return Array.from(map).reduce((acc, curr) => (curr[1] + 1) * acc, 1) - 1;
 }
